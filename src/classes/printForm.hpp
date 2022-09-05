3aceb8ac4cd8be6564c72617eee63d480a6427ba
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-09-05 14:00:48 -0600

    feat: Added adaptive printing to printForm - Added in methods "getXBorder" - Added methods "printXBorder" for direct printing

--- src/classes/printForm.hpp
+++ src/classes/printForm.hpp
@@ -11,15 +11,17 @@
 
 using namespace std;
 
-void addLine(string &baseString, int indentLevel, string addString1, string addString2)
+string addLine(string &baseString, int indentLevel, string addString1, string addString2)
 {
   if (indentLevel < 1)
   {
     baseString.append("\n\r\u2503  ");
     if (indentLevel == -1)
     {
-      baseString.append("\e[80G\u2503");
-      return;
+      baseString.append("\e[1G\e[80C\u2503");
+      string copyString = baseString;
+
+      return copyString;
     }
   }
   else if (indentLevel == 1)
@@ -40,30 +42,86 @@ void addLine(string &baseString, int indentLevel, string addString1, string addS
   }
   baseString.append(addString1);
   baseString.append(addString2);
-  baseString.append("\e[80G\u2503");
+  baseString.append("\e[1G\e[80C\u2503");
 
-  return;
+  string copyString = baseString;
+
+  return copyString;
 }
 
-// void addLine(string &baseString, int indentLevel, string addString)
-// {
-//   addLine(baseString, indentLevel, addString, "");
-//   return;
-// }
+string getTopBorder()
+{
+  string topBorder = "\n\r\u250F";
+  for (int i = 0; i < 80; i++)
+  {
+    topBorder.append("\e[1D\e[1C\u2501");
+  }
+  topBorder.append("\e[1D\e[1C\u2513");
+  return topBorder;
+}
 
-// void addLine(string &baseString, int indentLevel)
-// {
-//   addLine(baseString, indentLevel, "");
-//   return;
-// }
+string getInnerBorder()
+{
+  string innerBorder = "\n\r\u2520";
+  for (int i = 0; i < 80; i++)
+  {
+    innerBorder.append("\e[1D\e[1C\u2500");
+  }
+  innerBorder.append("\e[1D\e[1C\u2528");
+  return innerBorder;
+}
 
-// void addLine(string &baseString)
-// {
-//   addLine(baseString, -1);
-//   return;
-// }
+string getThickInnerBorder()
+{
+  string innerBorder = "\n\r\u2523";
+  for (int i = 0; i < 80; i++)
+  {
+    innerBorder.append("\e[1D\e[1C\u2501");
+  }
+  innerBorder.append("\e[1D\e[1C\u252B");
+  return innerBorder;
+}
+
+string getBottomBorder()
+{
+  string bottomBorder = "\n\r\u2517";
+  for (int i = 0; i < 80; i++)
+  {
+    bottomBorder.append("\e[1D\e[1C\u2501");
+  }
+  bottomBorder.append("\e[1D\e[1C\u251B");
+  return bottomBorder;
+}
+
+void printTopBorder()
+{
+  cout << getTopBorder();
+}
+
+void printInnerBorder()
+{
+  cout << getInnerBorder();
+}
+
+void printThickInnerBorder()
+{
+  cout << getThickInnerBorder();
+}
+
+void printBottomBorder()
+{
+  cout << getBottomBorder();
+}
 
 #ifndef PRINTFORM
 #define PRINTFORM
-void addLine(string &baseString, int indentLevel = 0, string addString1 = "", string addString2 = "");
+string addLine(string &baseString, int indentLevel = 0, string addString1 = "", string addString2 = "");
+string getTopBorder();
+string getInnerBorder();
+string getThickInnerBorder();
+string getBottomBorder();
+void printTopBorder();
+void printInnerBorder();
+void printThickInnerBorder();
+void printBottomBorder();
 #endif // PRINTFORM
